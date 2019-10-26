02b5873ea353e307807272b3be9cb48eed5b7b3d
fernanda1810 <37128686+fernanda1810@users.noreply.github.com>
2019-10-26 13:43:39 -0600

    small fixes

--- DiarioFacil/src/edu/ulatina/patrones/diarioFacil/ProductoTester.java
+++ DiarioFacil/src/edu/ulatina/patrones/diarioFacil/ProductoTester.java
@@ -25,8 +25,10 @@ public class ProductoTester  {
         for (int i = 0; i < lstProductos.size(); i++) {
             System.out.println(lstProductos.get(i).getId()+" "+lstProductos.get(i).getNombreProd()+" "+lstProductos.get(i).getPrecio());
         }
+        
             //System.out.println(productodao.get(2).get().getId()+productodao.get(2).get().getNombreProd());   
-    
+            
+            //Aqui creo un nuevo producto en la base de datos
             /*Producto newProducto = new Producto();
             System.out.println("Ingrese el numbre del producto");
             newProducto.setNombreProd(scanner.next());
@@ -34,11 +36,13 @@ public class ProductoTester  {
             newProducto.setPrecio(scanner.nextDouble());
             productodao.save(newProducto);*/
             
+            //Esta elimina un producto mediante el id del producto como unica referencial al objeto que deseo eliminar.
             /*Producto deleteProducto= new Producto();
             System.out.println("Ingrese el id del producto a eliminar");
             deleteProducto.setId(scanner.nextInt());
             productodao.delete(deleteProducto);*/
             
+            //Aqui eligo el id del producto a cambiar para despues mandar sql statement y cambiarle los cambios asignados en producto dao
             Producto updateProducto= new Producto();
             System.out.println("Ingrese el id del producto a actualizar");
             updateProducto.setId(scanner.nextInt());
@@ -48,8 +52,10 @@ public class ProductoTester  {
             updateProducto.setPrecio(scanner.nextDouble());
             productodao.update(updateProducto, args);
             
+            
+            //Aqui me traigo la lista nuevamente para ver el cambio de modificar
             lstProductos = productodao.getAll();
-        
+        //Recorro la lista
         for (int i = 0; i < lstProductos.size(); i++) {
             System.out.println(lstProductos.get(i).getId()+" "+lstProductos.get(i).getNombreProd()+" "+lstProductos.get(i).getPrecio());
         }
