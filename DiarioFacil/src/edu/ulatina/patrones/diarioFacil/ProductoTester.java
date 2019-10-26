8f81baa6b20582f1aa070799de1328212ad77814
fernanda1810 <37128686+fernanda1810@users.noreply.github.com>
2019-10-26 13:35:41 -0600

    Producto Details

--- DiarioFacil/src/edu/ulatina/patrones/diarioFacil/ProductoTester.java
+++ DiarioFacil/src/edu/ulatina/patrones/diarioFacil/ProductoTester.java
@@ -1,12 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+
 package edu.ulatina.patrones.diarioFacil;
 
+import static java.lang.System.in;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class ProductoTester  {
@@ -14,6 +14,7 @@ public class ProductoTester  {
     
     
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(in);
        ProductoDao productodao = new ProductoDao();
         List<Producto> lstProductos = new ArrayList<>();
         
@@ -22,12 +23,37 @@ public class ProductoTester  {
         lstProductos = productodao.getAll();
         
         for (int i = 0; i < lstProductos.size(); i++) {
-            System.out.println(lstProductos.get(i).getNombreProd());
-            
-            
+            System.out.println(lstProductos.get(i).getId()+" "+lstProductos.get(i).getNombreProd()+" "+lstProductos.get(i).getPrecio());
         }
+            //System.out.println(productodao.get(2).get().getId()+productodao.get(2).get().getNombreProd());   
+    
+            /*Producto newProducto = new Producto();
+            System.out.println("Ingrese el numbre del producto");
+            newProducto.setNombreProd(scanner.next());
+            System.out.println("Ingrese el precio del producto");
+            newProducto.setPrecio(scanner.nextDouble());
+            productodao.save(newProducto);*/
+            
+            /*Producto deleteProducto= new Producto();
+            System.out.println("Ingrese el id del producto a eliminar");
+            deleteProducto.setId(scanner.nextInt());
+            productodao.delete(deleteProducto);*/
+            
+            Producto updateProducto= new Producto();
+            System.out.println("Ingrese el id del producto a actualizar");
+            updateProducto.setId(scanner.nextInt());
+            System.out.println("Ingrese el nombre del producto a actualizar");
+            updateProducto.setNombreProd(scanner.next());
+            System.out.println("Ingrese el precio del producto a actualizar");
+            updateProducto.setPrecio(scanner.nextDouble());
+            productodao.update(updateProducto, args);
+            
+            lstProductos = productodao.getAll();
         
-            System.out.println(productodao.get(2).get().getId()+productodao.get(2).get().getNombreProd());   
+        for (int i = 0; i < lstProductos.size(); i++) {
+            System.out.println(lstProductos.get(i).getId()+" "+lstProductos.get(i).getNombreProd()+" "+lstProductos.get(i).getPrecio());
+        }
     }
+    
     
 }
