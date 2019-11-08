abe184a9def4f6c14900c577c0bb4c8a53f0d611
CHAVEZ RIVAS <jese.chavez@ulatina.net>
2019-11-07 22:25:22 -0600

    Inventario y carrito (Caracteristicas pendientes) -Crear una orden -Agregar al carrito -Comprar el carrito -Finalizar orden

--- DiarioFacil/src/edu/ulatina/patrones/diarioFacil/Tester.java
+++ DiarioFacil/src/edu/ulatina/patrones/diarioFacil/Tester.java
@@ -16,26 +16,8 @@ public class Tester {
 
   
     
-   public static void main(String[] args) {
-       MenuProducto menuProducto= new MenuProducto();
-       menuProducto.menu();
-
-      // MenuCategorias categorias=new MenuCategorias();
-      // categorias.menu();
-       
-
-      /*MenuProveedores proveedores=new MenuProveedores();
-     proveedores.menu();*/
-
-//      MenuProveedores proveedores=new MenuProveedores();
-//     proveedores.menu();
-
-
-       //MenuCategorias categorias=new MenuCategorias();
-       //categorias.menu();
-//       ClienteDao dao  = new ClienteDao();
-//       dao.save( new Cliente("XXXX","XXXXXXPASS","CDF",false,false));
-      /* try {
+   public static void main(String[] args) {      
+       try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -44,14 +26,13 @@ public class Tester {
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Tester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }*/
-       /*MenuLogin log = new MenuLogin();
-       log.loginUI();*/
-     // MenuProveedores proveedores=new MenuProveedores();
-     // proveedores.menu();
+        }
+       MenuLogin log = new MenuLogin();
+       log.loginUI();
 
-    }
-   // 
+       
+   }
+   
 }
     
     
